45ba07fc5c5dc222229743b0fe63ef13c6376907
Scott Fielder <scottkfielder@gmail.com>
2014-04-23 14:58:19 -0400

    updated home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HerokuConnectRails::Application.routes.draw do
   resources :accounts
-  root 'account#index'
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
